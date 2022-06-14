059dcf059297d8cd371f6dce910f8d6223d917df
Юрий Пирязев <ypiryazev@gmail.com>
2022-06-14 20:49:01 +0700

    process changes

---
+++ Process.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace OperationSystemProject
+{
+    class Process
+    {
+        private int         id;
+        private List<Frame> frameList;
+
+    }
+}
